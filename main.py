44756aff1aceafff2bce243eea833fe600c211c1
Vijayant Prateek <62292932+Unknown1770@users.noreply.github.com>
2021-03-22 20:27:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from flask import Flask,request
+from datetime import time
+
+import firebase_admin
+import requests
+from firebase_admin import credentials
+from firebase_admin import firestore
+import flask
+from flask import request, jsonify, Flask, render_template
+
+# initialize firebase application
+#firebase_admin.initialize_app()
+cred = credentials.Certificate('testing-bf5a4-firebase-adminsdk-k3wvf-9481825d20.json')
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://testing-bf5a4.firebaseio.com/'
+})
+
+db = firestore.client()
+
+
 
 app = Flask(__name__)
 # GET requests will be blocked
@@ -46,8 +65,30 @@ def profile_example():
             
         if 'name' in request_data:
             name = request_data['name']
+            
+        user_name = name
+        addressal_name = name
+            
+        docref = db.collection('Profile').document()
+        data1 = {
+           'city': city,
+           'email': email,
+           'name': name,
+           'mobile': mobile,
+           'gender': gender,
+           'dob': dob,
+           'user_name' : user_name,
+           'addressal_name' : addressal_name,
+           'dob_year': dob_year,
+           'dob_month' : dob_month
+           'nationality': nationality
+         }
+        
+        docref.set(data1)  
+        ldoc_id = docref.id
 
     return '''
+           Document_ID = {}
            Name: {}
            City: {}
            Email: {}
@@ -55,7 +96,7 @@ def profile_example():
            Date_of_birth: {}
            Birth_month: {}
            Birth_year: {}
-           Gender: {}'''.format(name, city, email, mobile, dob, dob_month, dob_year, gender)
+           Gender: {}'''.format(ldoc_id,name, city, email, mobile, dob, dob_month, dob_year, gender)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
